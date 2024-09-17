0459605cf792a4577a72d69e3b4bb290b39728ec
Nicolas <you@example.com>
2024-09-16 21:05:36 -0500

    Initial commit

---
+++ MyQueue/MyQueue.cs
@@ -0,0 +1,53 @@
+﻿using System;
+    public class MyQueue<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public void Enqueue(T item)
+        {
+            Node<T> node = new Node<T>(item);
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                tail.setNext(node);
+                tail = node;
+            }
+               
+        }
+
+
+        public T Dequeue()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            T data = head.getData();
+            head = head.getNext();
+
+            if (head == null)
+            {
+                tail = null;
+            }
+            return data;
+        }
+
+        public T Peek()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+            return head.getData();
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+    }
---
+++ MyQueue/Prueba.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyQueue
+{
+    public class Prueba
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TEST QUEUE");
+
+            MyQueue<String> myQueue = new MyQueue<string>();
+            myQueue.Enqueue("A");
+            
+            
+            Console.WriteLine(myQueue.IsEmpty());
+            Console.WriteLine(myQueue.Dequeue());
+            Console.WriteLine(myQueue.Peek());
+        }
+    }
+}
